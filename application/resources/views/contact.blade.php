a9eca277e9edc0c3f05546a362b61382c8884ca8
Willton <willtondacosta@gmail.com>
2024-09-18 17:48:02 +0200

    primeiras aulas de blade

---
+++ application/resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Esta e a pagina de contacto</h1>
+<a href="/">Voltar para Home</a>
--- application/routes/web.php
+++ application/routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $idade = 24;
+    $nome  = "Willas";
+
+    $arr = [1,2,3,4,5];
+
+    $nomes = ["Mateus", "Maria", "Jonathan", "Paulo"];
+
+    return view('welcome', 
+    [
+        "nome" => $nome, 
+        "idade" => $idade, "profissao" => "Programador",
+        "arr" => $arr,
+        "nomes" => $nomes
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('contact');
 });
